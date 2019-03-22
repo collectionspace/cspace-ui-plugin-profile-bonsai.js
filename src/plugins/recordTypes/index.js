03e21bb9f7bb94c705af0569d4eb9648e3be8225
Ray Lee <ray.lee@lyrasis.org>
2019-03-22 16:09:43 -0700

    DRYD-195: Implement exhibition customizations.

--- src/plugins/recordTypes/index.js
+++ src/plugins/recordTypes/index.js
@@ -1,11 +1,13 @@
 import collectionobject from './collectionobject';
 import conditioncheck from './conditioncheck';
 import conservation from './conservation';
+import exhibition from './exhibition';
 import objectexit from './objectexit';
 
 export default [
   collectionobject,
   conditioncheck,
   conservation,
+  exhibition,
   objectexit,
 ];
