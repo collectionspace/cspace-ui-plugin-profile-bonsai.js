11906aa309cb1b7fd012788776645672a0de6ab5
Mark Cooper <markchristophercooper@gmail.com>
2019-10-16 16:34:35 -0700

    Add values to objectexit optionslists

--- src/plugins/recordTypes/objectexit/optionLists.js
+++ src/plugins/recordTypes/objectexit/optionLists.js
@@ -1,10 +1,26 @@
 import { defineMessages } from 'react-intl';
 
 export default {
+  exitMethods: {
+    values: [
+      'courier',
+      'garbage',
+      'inperson',
+      'post',
+    ],
+    messages: defineMessages({
+      garbage: {
+        id: 'option.exitMethods.garbage',
+        defaultMessage: 'garbage',
+      },
+    }),
+  },
   exitReasons: {
     values: [
+      'broken',
       'deaccession',
       'dead',
+      'deadandbroken',
       'disposal',
       'fundraiser',
       'missing',
@@ -12,9 +28,17 @@ export default {
       'returnofloan',
     ],
     messages: defineMessages({
+      broken: {
+        id: 'option.exitReasons.broken',
+        defaultMessage: 'broken',
+      },
       dead: {
         id: 'option.exitReasons.dead',
         defaultMessage: 'dead',
+      },
+      deadandbroken: {
+        id: 'option.exitReasons.deadandbroken',
+        defaultMessage: 'dead and broken',
       },
       fundraiser: {
         id: 'option.exitReasons.fundraiser',
